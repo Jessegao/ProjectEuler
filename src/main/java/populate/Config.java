6348b59657d4879e45e8b2cbe727ed308c08deac
jessegao <jessegao12@gmail.com>
2016-11-07 19:26:18 -0800

    Euler

---
+++ src/main/java/populate/Config.java
@@ -0,0 +1,10 @@
+package populate;
+
+/**
+ * Global class used to change world parameters.
+ */
+public class Config {
+    public static int ACTOR_COUNT = 20;
+    public static int LIMIT = 1000; //number you want to sum up to
+    public static int[] NUMBERS = {3, 5}
+}
